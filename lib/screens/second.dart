4f4d6569ba3b9cbda86e5b02f90373b1e0acd0e2
AbdiwakBekele <abdiwakbek3226@gmail.com>
2023-10-24 16:52:47 +0300

    Drawr included

--- lib/screens/second.dart
+++ lib/screens/second.dart
@@ -11,12 +11,25 @@ class Second extends StatelessWidget {
       appBar: AppBar(
         title: Text("Result"),
       ),
-      body: Center(
-        child: Text(
-          result.toString(),
-          style: TextStyle(color: res_color),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            ListTile(
+              leading: Icon(Icons.add_box),
+              title: Text("About"),
+              subtitle: Text('this is About'),
+              trailing: Icon(Icons.check),
+              onTap: () {},
+            )
+          ],
         ),
       ),
+      body: Center(
+        child: Image.network(
+            "https://images.sidearmdev.com/crop?url=https%3a%2f%2fdxbhsrqyrr690.cloudfront.net%2fsidearm.nextgen.sites%2fuconnhuskies.com%2fimages%2f2023%2f10%2f21%2fUConnFB_vs_USF_20231021_CM0_3018.jpg&height=608&width=1080&type=jpeg&gravity=smart"),
+      ),
+      floatingActionButton:
+          FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
     );
   }
 }
